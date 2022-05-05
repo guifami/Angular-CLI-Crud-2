85cf85dbcf71f8d5b06f427cd4d64c4ebe18117c
Guilherme Ruiz da Silva <guiruiz05112003@gmail.com>
2022-05-05 08:31:52 -0300

    First Deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { DialogComponent } from './dialog/dialog.component'
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ApiService } from './services/api.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular13Crud';
+
+  displayedColumns: string[] = ['productName', 'category', 'date', 'freshness', 'price', 'comment', 'action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private dialog: MatDialog, private api: ApiService) {
+
+  }
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+    }).afterClosed().subscribe(value => {
+      if(value === 'Salvar'){
+        this.getAllProducts();
+      }
+    })
+  }
+
+  getAllProducts(){
+    this.api.getProduct().subscribe({
+      next:(res)=>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error:(err)=>{
+        alert("Erro ao buscar os Registros.");
+      }
+    })
+  }
+
+  editProduct(row : any){
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+      data: row
+    }).afterClosed().subscribe(value => {
+      if(value === "Alterar"){
+        this.getAllProducts();
+      }
+    })
+  }
+  deleteProduct(id : number){
+    this.api.deleteProduct(id).subscribe({
+      next: (result) => {
+        alert("Produto deletado com Sucesso.");
+        this.getAllProducts();
+      },
+      error: (err) => {
+        alert("Erro ao deletar um Registro.");
+      }
+    })
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
 }
